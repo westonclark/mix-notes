4b0ce7b4b099ab336faf978b67ec9ead67a369a4
Weston Clark <westoclark@gmail.com>
2023-06-10 20:16:20 -0700

    split main component into smaller components

--- src/server/api/routers/projects.ts
+++ src/server/api/routers/projects.ts
@@ -2,6 +2,9 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const projectsRouter = createTRPCRouter({
+
+
+  
   getProjects: publicProcedure.input(z.string()).query(({ ctx, input }) => {
     return ctx.prisma.project.findMany({
       where: { user: input },
