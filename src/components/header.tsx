e1aaf1ac535a5350db0375a8f0d05f65bd772ae3
Weston Clark <westoclark@gmail.com>
2023-07-07 09:11:24 -0700

    fix header padding for mobile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 flex items-center justify-between border-b border-neutral-400 bg-neutral-900 px-40 py-4 text-white-1">
+    <header className="sticky top-0 flex items-center justify-between border-b border-neutral-400 bg-neutral-900 md:px-40 p-4 text-white-1">
       <Link className="text-xl" href="/">
         MIX NOTES
       </Link>
